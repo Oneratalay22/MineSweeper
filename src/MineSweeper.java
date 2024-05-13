f2a1e7f5b88e6de6b5c79245b989d1239eb2957c
Öner Atalay <165701610+Oneratalay22@users.noreply.github.com>
2024-05-13 10:12:01 +0300

    fix

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -58,37 +58,37 @@ public class MineSweeper {
         if (map[r][c] == 0) {
         }
         // Sağdaki hücrede mayın varsa
-        if ((c < colNumber - 1) && (map[r][c + 1] == -1)) {
+        if ((c < colNumber - 1) && (map[r][c + 1] == '*')) {
             board[r][c]++;
         }
         // Soldaki hücrede mayın varsa
-        if ((r < rowNumber - 1) && (map[r + 1][c] == -1)) {
+        if ((r < rowNumber - 1) && (map[r + 1][c] == '*')) {
             board[r][c]++;
         }// Üstteki hücrede mayın varsa
-        if ((r > 0) && (map[r - 1][c] == -1)) {
+        if ((r > 0) && (map[r - 1][c] == '*')) {
             board[r][c]++;
         }
         // Alttaki hücrede mayın varsa
-        if ((c > 0) && (map[r][c - 1] == -1)) {
+        if ((c > 0) && (map[r][c - 1] == '*')) {
             board[r][c]++;
         }// Sağ üst çapraz hücrede mayın varsa
-        if (r > 0 && c < colNumber - 1 && map[r - 1][c + 1] == -1) {
+        if (r > 0 && c < colNumber - 1 && map[r - 1][c + 1] == '*') {
             board[r][c]++;
         }
         // Sol üst çapraz hücrede mayın varsa
-        if (r > 0 && c > 0 && map[r - 1][c - 1] == -1) {
+        if (r > 0 && c > 0 && map[r - 1][c - 1] == '*') {
             board[r][c]++;
         }
         // Sağ alt çapraz hücrede mayın varsa
-        if (r < rowNumber - 1 && c < colNumber - 1 && map[r + 1][c + 1] == -1) {
+        if (r < rowNumber - 1 && c < colNumber - 1 && map[r + 1][c + 1] == '*') {
             board[r][c]++;
         }
         // Sol alt çapraz hücrede mayın varsa
-        if (r < rowNumber - 1 && c > 0 && map[r + 1][c - 1] == -1) {
+        if (r < rowNumber - 1 && c > 0 && map[r + 1][c - 1] == '*') {
             board[r][c]++;
         }
         if (board[r][c] == 0) {
-            board[r][c] = '*';
+            board[r][c] = '-';
         }
     }
 
@@ -99,8 +99,8 @@ public class MineSweeper {
             randRow = rand.nextInt(rowNumber);
             randCol = rand.nextInt(colNumber);
             // Eğer belirlenen hücrede daha önce mayın yoksa
-            if (map[randRow][randCol] != -1) {
-                map[randRow][randCol] = -1;
+            if (map[randRow][randCol] != '*') {
+                map[randRow][randCol] = '*';
                 count++;
             }
         }
